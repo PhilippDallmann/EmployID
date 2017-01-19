1b3eeda5fb350a1a79fb355df620661332392662
PhilippDallmann <philipp.dallmann@outlook.com>
2017-01-19 17:18:32 +0100

    - added import logic to make collection publishes available

---
+++ imports/startup/server/register-api.js
@@ -0,0 +1,8 @@
+import '../../api/chatMessages/server/publications';
+import '../../api/chats/server/publications';
+import '../../api/groups/server/publications';
+import '../../api/materials/server/publications';
+import '../../api/meetings/server/publications';
+import '../../api/notifications/server/publications';
+import '../../api/stageMessages/server/publications';
+import '../../api/stages/server/publications';
