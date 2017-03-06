d45d4dd8ffcd5484c10850976caa821a24971157
PhilippDallmann <philipp.dallmann@outlook.com>
2017-03-07 00:41:25 +0100

    -added mup files

---
+++ .deploy/mup.js
@@ -0,0 +1,46 @@
+module.exports = {
+  servers: {
+    one: {
+      host: '1.2.3.4',
+      username: 'root',
+      // pem:
+      // password:
+      // or leave blank for authenticate from ssh-agent
+    }
+  },
+
+  meteor: {
+    name: 'app',
+    path: '../app',
+    servers: {
+      one: {},
+    },
+    buildOptions: {
+      serverOnly: true,
+    },
+    env: {
+      ROOT_URL: 'app.com',
+      MONGO_URL: 'mongodb://localhost/meteor',
+    },
+
+
+    docker: {
+      // change to 'kadirahq/meteord' if your app is not using Meteor 1.4
+      image: 'abernix/meteord:base',
+    },
+    deployCheckWaitTime: 60,
+
+    // Show progress bar while uploading bundle to server
+    // You might need to disable it on CI servers
+    enableUploadProgressBar: false
+  },
+
+  mongo: {
+    oplog: true,
+    port: 27017,
+    version: '3.4.1',
+    servers: {
+      one: {},
+    },
+  },
+};
