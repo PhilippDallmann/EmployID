f357e3b5e39070dce17a44d5cd43a2956cdb30cc
PhilippDallmann <philipp.dallmann@outlook.com>
2017-08-15 22:18:38 +0200

    - added es linting packages

--- imports/api/rest/restapi.js
+++
@@ -1,9 +0,0 @@
-import {Meteor} from 'meteor/meteor';
-import {Restivus} from 'meteor/nimble:restivus';
-
-if(Meteor.isServer) {
-  var Api = new Restivus({
-    useDefaultAuth: true,
-    prettyJson: true
-  });
-}
