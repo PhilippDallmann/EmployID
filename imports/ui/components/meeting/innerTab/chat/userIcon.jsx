845fd6b593c87dcd675332d6976279b7fe7e3a94
PhilippDallmann <philipp.dallmann@outlook.com>
2017-08-18 00:21:10 +0200

    - removed utils folder

--- imports/ui/components/meeting/innerTab/chat/userIcon.jsx
+++ imports/ui/components/meeting/innerTab/chat/userIcon.jsx
@@ -1,7 +1,5 @@
 import React, {Component, PropTypes} from 'react';
 
-import truncate from "../../../../../util/truncate";
-
 let defaultUserImage = '/img/defaultUserImage_140x140.png';
 
 let UserIcon = React.createClass({
@@ -10,7 +8,7 @@ let UserIcon = React.createClass({
           <div ref="target" className="userIcon">
               <img className={this.props.status} src= {(this.props.user.profile && this.props.user.profile.avatar)? this.props.user.profile.avatar : defaultUserImage} />
               <div className={this.props.pulse} id={this.props.user.username}></div>
-              <span>{truncate(this.props.spanText, 7)}</span>
+              <span>{this.props.spanText}</span>
           </div>
         );
     }
